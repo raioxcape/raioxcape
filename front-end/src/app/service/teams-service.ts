d9dd0e2d1977efd3764905269b03ceb399b06462
Marcos Visentini <marcosvisentini@gmail.com>
2023-06-19 22:00:37 -0300

    Realiza alterações necessárias no front-end para deploy

--- front-end/src/app/service/teams-service.ts
+++ front-end/src/app/service/teams-service.ts
@@ -4,26 +4,28 @@ import { HttpClient,  HttpHeaders} from "@angular/common/http";
 import { Observable, delay, tap } from "rxjs";
 import { Equipe } from "../classes/Equipe";
 
+import { environment } from "src/environments/environment";
+
 @Injectable({
-    providedIn: 'root'
-  })
+    providedIn: "root"
+})
 export class TeamsService {
     
-    baseUrl : string = "api/equipes";
+    endpoint: string = `${environment.apiBaseUrl}/equipes`;
     
-    constructor(private http : HttpClient){
+    constructor(private http: HttpClient) {
         
     }
 
     getTeams() {
-        return this.http.get<Equipe[]>(this.baseUrl)
+        return this.http.get<Equipe[]>(this.endpoint)
         .pipe(
-           // delay(5000),
+            // delay(5000),
             tap()
         );
     }
 
-    saveTeam(equipe : Equipe): Observable<any>{
-        return this.http.post(this.baseUrl, equipe);
+    saveTeam(equipe: Equipe): Observable<any> {
+        return this.http.post(this.endpoint, equipe);
     }
 }
---
+++ front-end/src/environments/environment.production.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: 'https://raioxcape-back-end.up.railway.app/api'
+};

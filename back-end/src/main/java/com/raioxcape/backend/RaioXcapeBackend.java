7203a35a099de905a7d8cf635e10e9a7dbf90470
Marcos Visentini <marcosvisentini@gmail.com>
2023-07-04 13:24:15 -0300

    Configura timezone para America/Sao_Paulo

--- back-end/src/main/java/com/raioxcape/backend/RaioXcapeBackend.java
+++ back-end/src/main/java/com/raioxcape/backend/RaioXcapeBackend.java
@@ -1,13 +1,22 @@
 package com.raioxcape.backend;
 
+import jakarta.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+
+import java.util.TimeZone;
 
 @SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class RaioXcapeBackend {
 
     public static void main(String[] args) {
         SpringApplication.run(RaioXcapeBackend.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
     }
 }
